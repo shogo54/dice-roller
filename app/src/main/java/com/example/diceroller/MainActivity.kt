481a1de0ba329c9625be45d0c0a2c6eb59d23882
Shogo Akiyama <sakiyama@knox.edu>
2021-06-13 15:29:52 -0700

    Add rolling dice functionality

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -11,5 +11,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            // Give a feedback to the user by a toast
+            // Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
